d8b1a5aa0b38b18ac9b810cb8b03469ed432a49a
Zach Frohardt <zfrohardt@gmail.com>
2021-04-23 11:41:11 -0700

    commit tree 1d927f37fd3791e5c2dd65bbb4cfcb1041836e93

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+class Animal {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, sex) {
+        super(name, sex);
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, sex) {
+        super(name, sex);
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird extends Animal {
+    constructor(name, sex) {
+        super(name, sex);
+    }
+
+    speak() {
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        }
+        return `${this.name} says squawk!`
+    }
+}
